d3a47e5cc7d6cfd1dab65b8deceb163a08d8619e
James Finlinson <the30clues@gmail.com>
2025-02-15 21:51:10 -0700

    Report the data insert commands used

---
+++ aisql/setupData-job_titles.sql
@@ -0,0 +1,10 @@
+INSERT INTO public.job_title (id, name, color, globally_accessible, group_ids, sequence, type, hourly_rate, company_id) VALUES
+('93409deb-de9d-4011-8c81-ada5e28851e8', 'General Manager', '#16537e', true, '{}', 0, 'salaried', null, 'f05756e8-496e-4586-95c8-4b21180739df'),
+('80e52450-362e-4341-a6d3-0482d0e3b29b', 'Project Manager', '#999999', true, '{}', 1, 'salaried', null, 'f05756e8-496e-4586-95c8-4b21180739df'),
+('505bc7f6-3915-4e5f-b273-79d781b8c508', 'Superintendent', '#6a329f', true, '{}', 2, 'salaried', null, 'f05756e8-496e-4586-95c8-4b21180739df'),
+('a978a765-6342-400c-ba49-c6f2b8c44d36', 'Foreman', '#2986cc', true, '{}', 3, 'hourly', null, 'f05756e8-496e-4586-95c8-4b21180739df'),
+('24c7fdf0-7834-43d7-95a5-df2c9c44b0bc', 'Journeyman 1', '#38761d', true, '{}', 4, 'hourly', null, 'f05756e8-496e-4586-95c8-4b21180739df'),
+('e2856af9-4ca3-43bc-b601-1c80e876c82c', 'Journeyman 2', '#6aa84f', true, '{}', 5, 'hourly', null, 'f05756e8-496e-4586-95c8-4b21180739df'),
+('726e95ba-2be9-4b96-a2e7-23f926ce64ff', 'Apprentice 1', '#bf9000', true, '{}', 6, 'hourly', null, 'f05756e8-496e-4586-95c8-4b21180739df'),
+('31637d61-48a7-48c5-ad52-9067d4e020aa', 'Apprentice 2', '#f1c232', true, '{}', 7, 'hourly', null, 'f05756e8-496e-4586-95c8-4b21180739df'),
+('145e82e3-f876-459d-9fe4-dcbe904f07a2', 'Laborer', '#e06666', true, '{}', 8, 'hourly', null, 'f05756e8-496e-4586-95c8-4b21180739df');
